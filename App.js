40fcd5f001eb9a936f3e1bc75d416a8019922f8a
koushal-1435 <kaushalendra1114@gmail.com>
2024-02-07 07:44:17 +0530

    main

--- App.js
+++ App.js
@@ -1,4 +1,13 @@
-const maindiv = React.createElement("h1", {id:"mainDiv"} ,[ React.createElement("p", {className:"childdiv"} , "this is the data"), React.createElement("p", {className:"childdiv"} , "this is the data"), React.createElement("p", {className:"childdiv"} , "this is the data")]);
+const manyChild = [];
+
+for(i = 0; i<=5; i++){
+  manyChild.push(
+    React.createElement("p",{key:i},"array data one")
+  )
+}
+
+
+const maindiv = React.createElement("h1", { id: "mainDiv" }, manyChild);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
